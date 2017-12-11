fb6189b2592ec7659f779b4bb4cc4790aa177402
Matt Winwood <mattwinwood@gmail.com>
2017-12-11 07:46:54 -0500

    Created state update listeners

--- src/App.js
+++ src/App.js
@@ -1,72 +1,161 @@
-// Getter and Setter
+import React from 'react';
+
 function createStore(reducer, initialState) {
     let state = initialState;
+    const listeners = [];
+
+    const subscribe = (listener) => (
+        listeners.push(listener)
+    );
 
     const getState = () => (state);
 
     const dispatch = (action) => {
         state = reducer(state, action);
+        listeners.forEach(l => l());
     };
 
     return {
+        subscribe,
         getState,
-        dispatch
+        dispatch,
     };
 }
 
-// What to do to the data
 function reducer(state, action) {
-    if ("ADD_MESSAGE" === action.type) {
+    if (action.type === 'ADD_MESSAGE') {
         return {
-            messages : state.messages.concat(action.message)
-        }
-    }
-    else if ("DELETE_MESSAGE" === action.type) {
+            messages: state.messages.concat(action.message),
+        };
+    } else if (action.type === 'DELETE_MESSAGE') {
         return {
-            messages : [
+            messages: [
                 ...state.messages.slice(0, action.index),
                 ...state.messages.slice(
                     action.index + 1, state.messages.length
                 ),
             ],
-        }
-
+        };
     } else {
         return state;
     }
 }
 
-const initialState = {messages : []};
+const initialState = {messages: []};
+
 const store = createStore(reducer, initialState);
 
-const addMessageAction1 = {
-    type : 'ADD_MESSAGE',
-    message : 'How does it look, Neil?',
+const listener = () => {
+    console.log('LISTENER 1Current state: ');
+    console.log(store.getState());
 };
 
-// Setter
-store.dispatch(addMessageAction1);
-// Getter
-const stateV1 = store.getState();
-const addMessageAction2 = {
-    type : 'ADD_MESSAGE', message : 'Looking good.',
+store.subscribe(listener);
+
+const addMessageAction1 = {
+    type: 'ADD_MESSAGE',
+    message: 'How do you read?',
 };
-// Setter
+store.dispatch(addMessageAction1);
+// -> `listener()` is called
+
+const addMessageAction2 = {
+    type: 'ADD_MESSAGE',
+    message: 'I read you loud and clear, Houston.',
+};
 store.dispatch(addMessageAction2);
-// Getter
-const stateV2 = store.getState();
-console.log('State v1:');
-console.log(stateV1);
-console.log('State v2:');
-console.log(stateV2);
+// -> `listener()` is called
 
 const deleteMessageAction = {
     type: 'DELETE_MESSAGE',
     index: 0,
 };
-
 store.dispatch(deleteMessageAction);
-const stateV3 = store.getState();
 
-console.log('State v3:');
-console.log(stateV3);
+// -> `listener()` is called
+
+class MessageInput extends React.Component {
+    state = {
+        value: '',
+    };
+    handleSubmit = () => {
+        store.dispatch({
+            type: 'ADD_MESSAGE',
+            message: this.state.value,
+        });
+        this.setState({
+            value: ''
+        });
+    }
+
+    onChange = (e) => {
+        this.setState({
+            value: e.target.value,
+        })
+    };
+
+    render() {
+        return (
+            <div className='ui input'>
+                <input
+                    onChange={this.onChange}
+                    value={this.state.value}
+                    type='text'
+                />
+                <button
+                    onClick={this.handleSubmit}
+                    className='ui primary button'
+                    type='submit'
+                >
+                    Submit
+                </button>
+            </div>
+        );
+    }
+}
+
+class MessageView extends React.Component {
+    handleClick = (index) => {
+        store.dispatch({
+            type: 'DELETE_MESSAGE',
+            index: index
+        });
+    };
+
+    render() {
+        const messages = this.props.messages.map((message, index) => (
+            <div
+                className='comment'
+                key={index}
+                onClick={() => this.handleClick(index)}
+            >
+                {message}
+            </div>
+        ));
+        return (
+            <div className='ui comments'>
+                {messages}
+            </div>
+        );
+    };
+}
+
+class App extends React.Component {
+    // setState with Redux managing data
+    componentDidMount() {
+        store.subscribe(() => this.forceUpdate());
+    }
+
+    render() {
+        const messages = store.getState().messages;
+
+        return (
+            <div className='ui segment'>
+                <MessageView messages={messages}/>
+                <MessageInput/>
+            </div>
+        );
+    }
+}
+
+export default App;
